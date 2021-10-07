a1afd906c8746767ae682da4da08127c9b2b48c5
Connor <conmmcguire@gmail.com>
2021-10-06 20:08:33 -0500

    cleaned up spacing

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -3,35 +3,27 @@ class Product
     @speed = 0
     @direction = 'north'
   end
-
   def brake
     @speed = 0
   end
-
   def accelerate
     @speed += 10
   end
-
   def turn(new_direction)
     @direction = new_direction
   end
-
 end
 
 class Car < Product
-  
   def honk_horn
     return "Beeeeeeep!"
   end
-
 end
 
 class Bike < Product
-
   def ring_bell
     return "Ring ring!"
   end
-
 end
 
 bike1 = Bike.new
